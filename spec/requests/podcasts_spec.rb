a52d3b96180fa0c5604ae3f890e960fcad23bda6
Alex Abramov <omgout.200@gmail.com>
2024-12-16 22:38:39 +0700

    remove #main

--- spec/requests/podcasts_spec.rb
+++ spec/requests/podcasts_spec.rb
@@ -3,21 +3,7 @@ require 'rails_helper'
 RSpec.describe "Podcasts", type: :request do
   describe "GET /index" do
     it "returns http success" do
-      get "/podcasts/index"
-      expect(response).to have_http_status(:success)
-    end
-  end
-
-  describe "GET /show" do
-    it "returns http success" do
-      get "/podcasts/show"
-      expect(response).to have_http_status(:success)
-    end
-  end
-
-  describe "GET /create" do
-    it "returns http success" do
-      get "/podcasts/create"
+      get root_path
       expect(response).to have_http_status(:success)
     end
   end
@@ -25,20 +11,6 @@ RSpec.describe "Podcasts", type: :request do
   describe "GET /new" do
     it "returns http success" do
       get "/podcasts/new"
-      expect(response).to have_http_status(:success)
-    end
-  end
-
-  describe "GET /edit" do
-    it "returns http success" do
-      get "/podcasts/edit"
-      expect(response).to have_http_status(:success)
-    end
-  end
-
-  describe "GET /destroy" do
-    it "returns http success" do
-      get "/podcasts/destroy"
       expect(response).to have_http_status(:success)
     end
   end
