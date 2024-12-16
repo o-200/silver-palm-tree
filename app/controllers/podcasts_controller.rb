352f71a693c6b3f718a449497424d7a9a77ff39a
Alex Abramov <omgout.200@gmail.com>
2024-12-16 20:49:38 +0700

    add worked CRUD for podcast

--- app/controllers/podcasts_controller.rb
+++ app/controllers/podcasts_controller.rb
@@ -10,17 +10,51 @@ class PodcastsController < ApplicationController
   end
 
   def show
-  end
-
-  def create
+    @podcast = Podcast.find_by(id: params[:id])
   end
 
   def new
+    @podcast = Podcast.new
+  end
+
+  def create
+    @podcast = Podcast.new(podcast_params)
+
+    if @podcast.save
+      redirect_to @podcast
+    else
+      render :new
+    end
   end
 
   def edit
+    @podcast = Podcast.find_by(id: params[:id])
+  end
+
+  def update
+    @podcast = Podcast.find(params[:id])
+
+    if @podcast.update(podcast_params)
+      redirect_to @podcast
+    else
+      render :edit
+    end
   end
 
   def destroy
+    @podcast = Podcast.find(params[:id])
+
+    if @podcast.destroy
+      redirect_to root_path
+    else
+      flash[:error] = "Podcast not found"
+      redirect_to @podcast
+    end
+  end
+
+  private
+
+  def podcast_params
+    params.require(:podcast).permit(:title, :description)
   end
 end
