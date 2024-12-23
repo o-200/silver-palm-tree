8f736a5f55962424a688d1252d56ac82c8573394
Alex Abramov <omgout.200@gmail.com>
2024-12-23 22:13:13 +0700

    remove render patrials

--- app/controllers/podcasts_controller.rb
+++ app/controllers/podcasts_controller.rb
@@ -6,13 +6,11 @@ class PodcastsController < ApplicationController
 
   def load_list
     podcasts = get_podcasts
-
     render partial: "podcasts_list", locals: { podcasts: podcasts }
   end
 
   def show
     @podcast = Podcast.find_by(id: params[:id])
-    render partial: "show", locals: { podcast: @podcast }
   end
 
   def new
@@ -23,7 +21,7 @@ class PodcastsController < ApplicationController
     @podcast = current_user.podcasts.build(podcast_params)
 
     if @podcast.save
-      redirect_to @podcast
+      render :show, locals: { podcast: @podcast }
     else
       render :new, notice: @podcast.errors.full_messages
     end
@@ -37,7 +35,7 @@ class PodcastsController < ApplicationController
     @podcast = Podcast.find(params[:id])
 
     if @podcast.update(podcast_params)
-      redirect_to @podcast
+      render :show, locals: { podcast: @podcast }
     else
       render :edit
     end
@@ -49,8 +47,8 @@ class PodcastsController < ApplicationController
     if @podcast.destroy
       redirect_to root_path
     else
-      flash[:error] = "Podcast not found"
-      redirect_to @podcast
+      flash[:error] = @podcast.errors.full_messages
+      render :show, locals: { podcast: @podcast }
     end
   end
 
@@ -61,7 +59,7 @@ class PodcastsController < ApplicationController
       return current_user.podcasts if params[:filter] == "my_podcasts"
     end
 
-    Podcast.take(12)
+    Podcast.all
   end
 
   def podcast_params
