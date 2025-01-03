a43d97bac6c609f50b3471815b0f91e9e83a2e9b
Alex Abramov <omgout.200@gmail.com>
2025-01-03 17:04:13 +0700

    take specific 'turbo-rails' and active-storage versions

--- Gemfile
+++ Gemfile
@@ -6,7 +6,7 @@ gem "sqlite3", ">= 1.4"
 
 gem "sprockets-rails"
 gem "importmap-rails"
-gem "turbo-rails"
+gem "turbo-rails", "~> 2.0"
 gem "stimulus-rails"
 gem "jbuilder"
 gem "cssbundling-rails", "~> 1.4"
--- config/importmap.rb
+++ config/importmap.rb
@@ -6,5 +6,5 @@ pin "@hotwired/stimulus", to: "stimulus.min.js"
 pin "@hotwired/stimulus-loading", to: "stimulus-loading.js"
 pin_all_from "app/javascript/controllers", under: "controllers"
 pin "bootstrap", to: "https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"
-pin "@rails/activestorage", to: "https://ga.jspm.io/npm:@rails/activestorage@7.0.2/app/assets/javascripts/activestorage.esm.js"
+pin "@rails/activestorage", to: "@rails--activestorage.js" # @8.0.100
 pin "@popperjs/core", to: "https://ga.jspm.io/npm:@popperjs/core@2.11.8/dist/umd/popper.min.js"
