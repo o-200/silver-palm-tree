5251d5ba70de09f4a631435cdf75a61744ee9bd8
krillan <Pushkinidze49@gmail.com>
2025-01-15 19:04:50 +0300

    added# initial versions of methods random and random_by_day to Podcst model

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,7 +2,8 @@ class HomeController < ApplicationController
   allow_unauthenticated_access
 
   def index
-    @random_podcast = Podcast.offset(rand(Podcast.count)).first # TODO: use cron
+    # @random_podcast = Podcast.offset(rand(Podcast.count)).first # TODO: use cron
+    @random_podcast = Podcast.random_by_day
     @last_podcast = Podcast.last
   end
 end
--- app/models/podcast.rb
+++ app/models/podcast.rb
@@ -1,7 +1,22 @@
 class Podcast < ApplicationRecord
+  RANDOM_BY_DAY_DATA = {id: 1, time: Time.current}
+
   has_one_attached :photo
   has_one_attached :audio
 
   validates :title, presence: true
   validates :title, length: { minimum: 2 }
+
+
+  scope :random_by_day, ->() do
+    return find(RANDOM_BY_DAY_DATA[:id]) if Time.current < RANDOM_BY_DAY_DATA[:time] + 1.minutes#1.days
+    new_random_podcast = random
+    RANDOM_BY_DAY_DATA[:id], RANDOM_BY_DAY_DATA[:time] = new_random_podcast.id, Time.current
+    new_random_podcast
+  end
+
+  scope :random, ->() do
+    offset(rand(Podcast.count)).first
+  end
+
 end
