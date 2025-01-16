86f53f17b234e6f08d4124c0290ac5e0d3f66be8
krillan <Pushkinidze49@gmail.com>
2025-01-16 16:50:34 +0300

    update# Rails.cache added to random_by_day method

--- app/models/podcast.rb
+++ app/models/podcast.rb
@@ -1,6 +1,4 @@
 class Podcast < ApplicationRecord
-  RANDOM_BY_DAY_DATA = {id: 1, time: Time.current}
-
   has_one_attached :photo
   has_one_attached :audio
 
@@ -9,10 +7,7 @@ class Podcast < ApplicationRecord
 
 
   scope :random_by_day, ->() do
-    return find(RANDOM_BY_DAY_DATA[:id]) if Time.current < RANDOM_BY_DAY_DATA[:time] + 1.minutes#1.days
-    new_random_podcast = random
-    RANDOM_BY_DAY_DATA[:id], RANDOM_BY_DAY_DATA[:time] = new_random_podcast.id, Time.current
-    new_random_podcast
+    find Rails.cache.fetch('random_podcast', expires_in: 1.minutes) { random.id }
   end
 
   scope :random, ->() do
