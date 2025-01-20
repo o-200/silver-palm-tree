0c8b1452926278b8e025d41b60ce92ba65207f10
krillan49 <126559400+krillan49@users.noreply.github.com>
2025-01-20 17:34:59 +0300

    5#random podcast by day (#21)

--- app/models/podcast.rb
+++ app/models/podcast.rb
@@ -4,4 +4,13 @@ class Podcast < ApplicationRecord
 
   validates :title, presence: true
   validates :title, length: { minimum: 2 }
+
+  def self.random_by_day
+    return unless exists?
+
+    cached_random_podcast_id = Rails.cache.fetch("random_podcast_id", expires_in: 1.days) do
+      offset(rand(Podcast.count)).first.id
+    end
+    find_by(id: cached_random_podcast_id)
+  end
 end
