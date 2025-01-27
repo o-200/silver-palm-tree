2f0b052906f9a12659131af4889da67d2586739f
krillan <Pushkinidze49@gmail.com>
2025-01-27 22:34:34 +0300

    added methods for podcast subscribe and unsubscribe

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,4 +8,12 @@ class User < ApplicationRecord
   normalizes :email_address, with: ->(e) { e.strip.downcase }
 
   validates :email_address, uniqueness: true
+
+  def subscribe_to_podcast(podcast)
+    subscriptions.create(podcast: podcast)
+  end
+  
+  def unsubscribe_from_podcast(podcast)
+    subscriptions.find_by(podcast: podcast)&.destroy
+  end
 end
