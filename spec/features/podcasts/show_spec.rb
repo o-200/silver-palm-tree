7ef64b2b88ce3bbae3f98d246592aff76d69384f
krillan <Pushkinidze49@gmail.com>
2025-01-31 22:42:18 +0300

    added authenticate and subscribe scenarios

--- spec/features/podcasts/show_spec.rb
+++ spec/features/podcasts/show_spec.rb
@@ -8,12 +8,11 @@ RSpec.feature "Podcasts#show", type: :feature do
                                 user: user)
   end
 
-  before do
-    sign_in
-    visit podcast_path(podcast)
-  end
+  context 'any user see' do
+    before do
+      visit podcast_path(podcast)
+    end
 
-  context 'user see' do
     scenario "podcast details" do
       expect(page).to have_content(podcast.title)
       expect(page).to have_content(podcast.description)
@@ -25,9 +24,55 @@ RSpec.feature "Podcasts#show", type: :feature do
       expect(page).to have_selector("div.alert.alert-warning", text: "No Audio Available!")
     end
 
+    scenario "subscribe button" do
+      expect(page).to have_button(id: "subscribe_button")
+    end
+  end
+
+  context 'unauthenticated user' do
+    before do
+      visit podcast_path(podcast)
+    end
+
+    scenario "does not see manipulation buttons" do
+      expect(page).not_to have_link("Редактировать Podcast", href: edit_podcast_path(podcast))
+      expect(page).not_to have_button("Удалить Podcast")
+    end
+
+    scenario "try to subscribe" do
+      click_button(id: "subscribe_button")
+
+      expect(page).to have_button("Sign in")
+    end
+  end
+
+  context 'authenticated user see' do
+    before do
+      sign_in
+      visit podcast_path(podcast)
+    end
+
     scenario "manipulation buttons" do
       expect(page).to have_link("Редактировать Podcast", href: edit_podcast_path(podcast))
       expect(page).to have_button("Удалить Podcast")
+    end
+  end
+
+  context 'subscriber see' do
+    before do
+      sign_in
+      visit podcast_path(podcast)
+      click_button(id: "subscribe_button")
+    end
+
+    scenario "unsubscribe button" do
+      expect(page).to have_button(id: "unsubscribe_button")
+    end
+
+    scenario "subscribe button" do
+      click_button(id: "unsubscribe_button")
+
+      expect(page).to have_button(id: "subscribe_button")
     end
   end
 end
